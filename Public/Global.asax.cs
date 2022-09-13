65085c04a0705d84add636cf9e0c6d20f958ebcb
david-basic <d.basic97.db@gmail.com>
2022-09-13 16:58:07 +0200

    global asax and web config updates

--- Public/Global.asax.cs
+++ Public/Global.asax.cs
@@ -1,8 +1,10 @@
+using Public.App_Start;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Public
@@ -13,6 +15,12 @@ namespace Public
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error()
+        {
+            Response.Redirect("~/Error/Index");
         }
     }
 }
