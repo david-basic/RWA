697a05a095fd4a12b8e9cc3638a7ce4a09b3b56f
david-basic <d.basic97.db@gmail.com>
2022-09-14 13:37:33 +0200

    some DB work done

--- Public/App_Start/Startup.cs
+++ Public/App_Start/Startup.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Owin;
+﻿using DataLayer.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
 using Owin;
+using Public.Models.Authentication;
 using System;
 using System.Threading.Tasks;
 
@@ -9,9 +14,27 @@ namespace Public.App_Start
 {
     public class Startup
     {
+        public string DefaultAuthType { get; private set; }
+
         public void Configuration(IAppBuilder app)
         {
-            
+            app.CreatePerOwinContext(DataBase.Create);
+            app.CreatePerOwinContext<UserManager>(UserManager.Create);
+            app.CreatePerOwinContext<SignInManager>(SignInManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions {
+                    AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                    LoginPath = new PathString("/Apartments/Index"),
+                    Provider = new CookieAuthenticationProvider {
+                        OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<UserManager, User>(
+                            validateInterval: TimeSpan.FromMinutes(30),
+                            regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager)
+                        )
+                    }
+                }
+            );
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
         }
     }
 }
---
+++ Public/Models/Authentication/SignInManager.cs
@@ -0,0 +1,31 @@
+﻿using DataLayer.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Public.Models.Authentication
+{
+    public class SignInManager : SignInManager<User, string>
+    {
+        public SignInManager(UserManager userManager, IAuthenticationManager authenticationManager) : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(User user)
+        {
+            return user.GenerateUserIdentityAsync((UserManager)UserManager);
+        }
+
+        public static SignInManager Create(IdentityFactoryOptions<SignInManager> options, IOwinContext owinContext)
+        {
+            return new SignInManager(owinContext.GetUserManager<UserManager>(), owinContext.Authentication);
+        }
+    }
+}
