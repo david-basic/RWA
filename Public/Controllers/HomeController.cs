f9548a7f13a66ef5cb0af9bdd02e14b903a30c51
david-basic <d.basic97.db@gmail.com>
2022-09-14 14:43:55 +0200

    login done, working on homepage

--- Public/Controllers/HomeController.cs
+++ Public/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Public.Models.Authentication;
+using Public.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +16,17 @@ namespace Public.Controllers
     public class HomeController : Controller
     {
         private UserManager _authManager;
+        private SignInManager _signInManager;
 
         public UserManager AuthManager
         {
             get { return _authManager ?? HttpContext.GetOwinContext().GetUserManager<UserManager>(); }
             set { _authManager = value; }
+        }
+        public SignInManager SignInManager
+        {
+            get { return _signInManager ?? HttpContext.GetOwinContext().Get<SignInManager>(); }
+            set { _signInManager = value; }
         }
 
         [HttpGet]
@@ -27,6 +34,33 @@ namespace Public.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public async Task<ActionResult> Index(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await AuthManager.FindAsync(model.Email, model.Password);
+
+            if (user != null)
+            {
+                await SignInManager.SignInAsync(user, true, model.RememberMe);
+
+                ViewBag.username = user.UserName;
+
+                return RedirectToAction(actionName: "Index", controllerName: "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Username or password is incorrect");
+                return View(model);
+            }
         }
 
         [HttpGet]
